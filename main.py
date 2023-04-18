6be8cbea79e62dc0128a8e2a4f2dfca267dd0b3e
PyryL <pyry@pyry.info>
2023-04-18 22:33:05 +0300

    acpi temperature

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ def get_cpu_temp():
         output["coretemp-isa-0000"]["Package id 0"]["temp1_max"]
     )
 
+def get_acpi_temp():
+    """ Returns (current, critical) tuple in Celsius """
+    output = subprocess.check_output("sensors -j", shell=True, text=True)
+    output = json.loads(output)
+    # TODO: handle multiple
+    return (
+        output["acpitz-acpi-0"]["temp1"]["temp1_input"],
+        output["acpitz-acpi-0"]["temp1"]["temp1_crit"]
+    )
+
 if __name__ == "__main__":
     cpu_temp = get_cpu_temp()
-    print(f"{cpu_temp[0]} / {cpu_temp[1]}")
+    acpi_temp = get_acpi_temp()
+    print(f" CPU: {cpu_temp[0]} / {cpu_temp[1]}")
+    print(f"ACPI: {acpi_temp[0]} / {acpi_temp[1]}")
