97456526c7d4adbfe65b7fcfa412bb2a9b2ddde2
PyryL <pyry@pyry.info>
2023-04-19 10:05:56 +0300

    made gui class more straight-forward

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ def main():
     cpu_temp = TempGetter.get_cpu_temp()
     acpi_temp = TempGetter.get_acpi_temp()
     graph = Grapher()
-    graph.add_graph(cpu_temp[0]/cpu_temp[1], "CPU", f"{cpu_temp[0]} / {cpu_temp[1]}")
-    graph.add_graph(acpi_temp[0]/acpi_temp[1], "ACPI", f"{acpi_temp[0]} / {acpi_temp[1]}")
-    graph.add_graph(112/87, "Test", f"{112} / {87}")
+    graph.add_graph("CPU", cpu_temp[0], cpu_temp[1])
+    graph.add_graph("ACPI", acpi_temp[0], acpi_temp[1])
+    graph.add_graph("Test", 112.48454884365865, 87)
     graph.print()
 
 if __name__ == "__main__":
--- temps/graph.py
+++ temps/graph.py
@@ -3,16 +3,18 @@ class Grapher:
     def __init__(self) -> None:
         self._graphs = []
 
-    def add_graph(self, percent: float, prefix: str, suffix: str) -> None:
-        self._graphs.append((percent, prefix, suffix))
+    def add_graph(self, title: str, current_temp: float, limit_temp: float) -> None:
+        self._graphs.append((current_temp, limit_temp, title))
 
     def print(self) -> None:
         bar_width = 12
-        prefix_length = max([len(t[1]) for t in self._graphs])
-        for (percent, prefix, suffix) in self._graphs:
-            padded_prefix = f"{prefix:<{prefix_length}}"
+        title_length = max([len(t[2]) for t in self._graphs])
+        for (current_temp, limit_temp, title) in self._graphs:
+            padded_title = f"{title:<{title_length}}"
+            percent = current_temp / limit_temp
             bar_char_count = round((100*percent) // (100/bar_width))
             bar_char_count = max(min(bar_char_count, bar_width), 0)
             bar_character = "#" if percent < 1 else "!"
             bar_str = "[" + bar_character*bar_char_count + " "*(bar_width-bar_char_count) + "]"
-            print(' '.join([padded_prefix, bar_str, suffix]))
+            suffix = f"{current_temp:3.0f} / {limit_temp:3.0f}"
+            print(' '.join([padded_title, bar_str, suffix]))
