cc0e24c36607c41ce13f30894b875f65f81c7af1
PyryL <pyry@pyry.info>
2023-04-19 09:52:11 +0300

    fixed negative percent gui bug

--- temps/graph.py
+++ temps/graph.py
@@ -12,7 +12,7 @@ class Grapher:
         for (percent, prefix, suffix) in self._graphs:
             padded_prefix = f"{prefix:<{prefix_length}}"
             bar_char_count = round((100*percent) // (100/bar_width))
-            bar_char_count = min(bar_char_count, bar_width)
+            bar_char_count = max(min(bar_char_count, bar_width), 0)
             bar_character = "#" if percent < 1 else "!"
             bar_str = "[" + bar_character*bar_char_count + " "*(bar_width-bar_char_count) + "]"
             print(' '.join([padded_prefix, bar_str, suffix]))
